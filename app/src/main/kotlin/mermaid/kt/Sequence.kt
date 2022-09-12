11a151a081278793fd5c0bf7243d7e84dbae9f8c
JeongSoo Lee <jeongsoolee@korea.ac.kr>
2022-09-12 11:03:55 -0700

    update

--- app/src/main/kotlin/mermaid/kt/Sequence.kt
+++ app/src/main/kotlin/mermaid/kt/Sequence.kt
@@ -1,5 +1,7 @@
 package mermaid.kt.Sequence
 
+class Unreachable() : Exception()
+
 /* ============ Components ============ */
 
 interface Component {
@@ -13,14 +15,13 @@ interface Component {
 
 abstract class Base : Component {
     override fun render(builder: StringBuilder, indent: String): Unit {
-        when (this) {}
     }
     override fun <T : Component> initComponent(component: T, thenWhat: T.() -> Unit) = component
 }
 
 // ============ Sub-Component Constructs ============
 
-abstract class SubComponent : Base()
+    abstract class SubComponent : Base()
 
 data class Actor(val name: String) : SubComponent()
 
@@ -29,13 +30,13 @@ fun actor(name: String) = Actor(name)
 class Color(val r: Int, val g: Int, val b: Int, val a: Float) : SubComponent() {
     companion object {
         fun fromString(colorString: String): Color =
-                when (colorString) {
-                    "red" -> Color(255, 0, 0, 0.2.toFloat())
-                    "green" -> Color(0, 255, 0, 0.1.toFloat())
-                    "blue" -> Color(0, 0, 255, 0.1.toFloat())
-                    "yellow" -> Color(255, 255, 0, 0.5.toFloat())
-                    "gray" -> Color(0, 0, 0, 0.1.toFloat())
-                    else -> Color(0, 0, 0, 0.toFloat()) // fall back to black.
+            when (colorString) {
+                "red" -> Color(255, 0, 0, 0.2.toFloat())
+                "green" -> Color(0, 255, 0, 0.1.toFloat())
+                "blue" -> Color(0, 0, 255, 0.1.toFloat())
+                "yellow" -> Color(255, 255, 0, 0.5.toFloat())
+                "gray" -> Color(0, 0, 0, 0.1.toFloat())
+                else -> Color(0, 0, 0, 0.toFloat()) // fall back to black.
                 }
         fun toString(color: Color): String = "rgb(${color.r}, ${color.g}, ${color.b}, ${color.a})"
     }
@@ -43,11 +44,11 @@ class Color(val r: Int, val g: Int, val b: Int, val a: Float) : SubComponent() {
 
 // ============ Labels ============
 
-abstract class Label() : Base() {
-    fun autonumber() = Autonumber()
+    abstract class Label() : Base() {
+        fun autonumber() = Autonumber()
 
-    fun participants(participants: List<Actor>) = Participants(participants)
-}
+        fun participants(participants: List<Actor>) = Participants(participants)
+    }
 
 class Autonumber() : Label()
 
@@ -57,47 +58,101 @@ data class Participants(var participants: List<Actor>?) : Label()
 
 abstract class Arrow() : Base()
 
-abstract class Call() : Arrow() {
-    fun callSync(from: Actor, to: Actor, message: String) = CallSync(from, to, message)
+abstract class Solid() : Arrow() {
+    fun line(from: Actor, to: Actor, message: String,
+             activate: Boolean = false, deactivate: Boolean = false) = SolidLine(from, to, message, activate, deactivate)
 
-    fun callActivate(
-            from: Actor,
-            to: Actor,
-            message: String,
-    ) = CallActivate(from, to, message)
+    fun arrow(from: Actor, to: Actor, message: String,
+              activate: Boolean = false, deactivate: Boolean = false) = SolidArrow(from, to, message, activate, deactivate)
 
-    fun callCross(from: Actor, to: Actor, message: String) = CallCross(from, to, message)
+    fun cross(from: Actor, to: Actor, message: String,
+              activate: Boolean = false, deactivate: Boolean = false) = SolidCross(from, to, message, activate, deactivate)
 
-    fun callAsync(from: Actor, to: Actor, message: String) = CallAsync(from, to, message)
+    fun openArrow(from: Actor, to: Actor, message: String,
+                  activate: Boolean = false, deactivate: Boolean = false) = SolidOpen(from, to, message, activate, deactivate)
 }
 
-class CallSync(var from: Actor?, var to: Actor?, var message: String?) : Call()
-
-class CallActivate(var from: Actor?, var to: Actor?, var message: String?) : Call()
-
-class CallCross(var from: Actor?, var to: Actor?, var message: String?) : Call()
-
-class CallAsync(var from: Actor?, var to: Actor?, var message: String?) : Call()
-
-// ============ Replys ============
-
-abstract class Reply() : Base() {
-    fun replySync(from: Actor, to: Actor, message: String) = ReplySync(from, to, message)
-
-    fun replyActivate(from: Actor, to: Actor, message: String) = ReplyActivate(from, to, message)
-
-    fun replyCross(from: Actor, to: Actor, message: String) = ReplyCross(from, to, message)
-
-    fun replyAsync(from: Actor, to: Actor, message: String) = ReplyAsync(from, to, message)
+class SolidLine(var from: Actor?, var to: Actor?, var message: String?,
+                val activate: Boolean = false, val deactivate: Boolean = false): Solid() {
+    override fun toString() : String =
+        when(Pair(this.activate, this.deactivate)) {
+            Pair(false, false) -> "${this.from}->${this.to}: ${this.message}"
+            Pair(true, false)  -> "${this.from}->+${this.to}: ${this.message}"
+            Pair(false, true)  -> "${this.from}->-${this.to}: ${this.message}"
+            Pair(true, true)   ->
+                throw IllegalArgumentException("Cannot be both activate and deactivate receiver at the same time")
+            else -> throw Unreachable()
+            }
 }
 
-class ReplySync(var from: Actor?, var to: Actor?, var message: String?) : Reply()
+class SolidArrow(var from: Actor?, var to: Actor?, var message: String?,
+                 val activate: Boolean = false, val deactivate: Boolean = false): Solid() {
+    override fun toString() : String =
+        when(Pair(this.activate, this.deactivate)) {
+            Pair(false, false) -> "${this.from}-->${this.to}: ${this.message}"
+            Pair(true, false)  -> "${this.from}-->+${this.to}: ${this.message}"
+            Pair(false, true)  -> "${this.from}-->-${this.to}: ${this.message}"
+            Pair(true, true)   ->
+                throw IllegalArgumentException("Cannot be both activate and deactivate receiver at the same time")
+            else -> throw Unreachable()
+            }
+}
 
-class ReplyActivate(var from: Actor?, var to: Actor?, var message: String?) : Reply()
+class SolidCross(var from: Actor?, var to: Actor?, var message: String?,
+                 val activate: Boolean = false, val deactivate: Boolean = false): Solid() {
+    override fun toString() : String =
+        when(Pair(this.activate, this.deactivate)) {
+            Pair(false, false) -> "${this.from}-x${this.to}: ${this.message}"
+            Pair(true, false)  -> "${this.from}-x+${this.to}: ${this.message}"
+            Pair(false, true)  -> "${this.from}-x-${this.to}: ${this.message}"
+            Pair(true, true)   ->
+                throw IllegalArgumentException("Cannot be both activate and deactivate receiver at the same time")
+            else -> throw Unreachable()
+            }
+}
 
-class ReplyCross(var from: Actor?, var to: Actor?, var message: String?) : Reply()
+class SolidOpen(var from: Actor?, var to: Actor?, var message: String?,
+                val activate: Boolean = false, val deactivate: Boolean = false): Solid() {
+    override fun toString() : String =
+        when(Pair(this.activate, this.deactivate)) {
+            Pair(false, false) -> "${this.from}-)${this.to}: ${this.message}"
+            Pair(true, false)  -> "${this.from}-)+${this.to}: ${this.message}"
+            Pair(false, true)  -> "${this.from}-)-${this.to}: ${this.message}"
+            Pair(true, true)   ->
+                throw IllegalArgumentException("Cannot be both activate and deactivate receiver at the same time")
+            else -> throw Unreachable()
+            }
+}
 
-class ReplyAsync(var from: Actor?, var to: Actor?, var message: String?) : Reply()
+abstract class Dotted() : Arrow() {
+    fun line(from: Actor, to: Actor, message: String,
+             activate: Boolean = false, deactivate: Boolean = false) =
+        DottedLine(from, to, message, activate, deactivate)
+
+    fun arrow(from: Actor, to: Actor, message: String,
+              activate: Boolean = false, deactivate: Boolean = false) =
+        DottedArrow(from, to, message, activate, deactivate)
+
+    fun cross(from: Actor, to: Actor, message: String,
+              activate: Boolean = false, deactivate: Boolean = false) =
+        DottedCross(from, to, message, activate, deactivate)
+
+    fun openArrow(from: Actor, to: Actor, message: String,
+                  activate: Boolean = false, deactivate: Boolean = false) =
+        DottedOpen(from, to, message, activate, deactivate)
+}
+
+class DottedLine(var from: Actor?, var to: Actor?, var message: String?,
+                 val activate: Boolean = false, val deactivate: Boolean = false): Dotted()
+
+class DottedArrow(var from: Actor?, var to: Actor?, var message: String?,
+                  val activate: Boolean = false, val deactivate: Boolean = false): Dotted()
+
+class DottedCross(var from: Actor?, var to: Actor?, var message: String?,
+                  val activate: Boolean = false, val deactivate: Boolean = false): Dotted()
+
+class DottedOpen(var from: Actor?, var to: Actor?, var message: String?,
+                 val activate: Boolean = false, val deactivate: Boolean = false): Dotted()
 
 /* ======================================== */
 /* ============ Inductive Case ============ */
@@ -125,28 +180,28 @@ abstract class Block() : Inductive() {
     fun loop(label: String, thenWhat: Loop.() -> Unit) = initComponent(Loop(label), thenWhat)
 
     fun noteLeft(actor: Actor, note: String, thenWhat: NoteLeft.() -> Unit) =
-            initComponent(NoteLeft(actor, note), thenWhat)
+        initComponent(NoteLeft(actor, note), thenWhat)
 
     fun noteRight(actor: Actor, note: String, thenWhat: NoteRight.() -> Unit) =
-            initComponent(NoteRight(actor, note), thenWhat)
+        initComponent(NoteRight(actor, note), thenWhat)
 
     fun noteOver(actor1: Actor, actor2: Actor, note: String, thenWhat: NoteOver.() -> Unit) =
-            initComponent(NoteOver(actor1, actor2, note), thenWhat)
+        initComponent(NoteOver(actor1, actor2, note), thenWhat)
 
     fun noteOver(actor: Actor, note: String, thenWhat: NoteOver.() -> Unit) =
-            initComponent(NoteOver(actor, null, note), thenWhat)
+        initComponent(NoteOver(actor, null, note), thenWhat)
 
     fun highlight(colorString: String, thenWhat: Highlight.() -> Unit) =
-            initComponent(Highlight(Color.fromString(colorString)), thenWhat)
+        initComponent(Highlight(Color.fromString(colorString)), thenWhat)
 
     fun alternative(condition: String, thenWhat: Alternative.() -> Unit) =
-            initComponent(Alternative(condition), thenWhat)
+        initComponent(Alternative(condition), thenWhat)
 
     fun parallel(description: String, thenWhat: Parallel.() -> Unit) =
-            initComponent(Parallel(description), thenWhat)
+        initComponent(Parallel(description), thenWhat)
 
     fun optional(description: String, thenWhat: Optional.() -> Unit) =
-            initComponent(Optional(description), thenWhat)
+        initComponent(Optional(description), thenWhat)
 }
 
 data class Loop(var label: String?) : Block()
@@ -161,100 +216,64 @@ data class Highlight(var color: Color?) : Block()
 
 class Alternative(var condition: String?) : Block() {
     fun elseClause(condition: String?, thenWhat: ElseClause.() -> Unit) =
-            initComponent(ElseClause(condition), thenWhat)
+        initComponent(ElseClause(condition), thenWhat)
 }
 
 class Parallel(var description: String?) : Block() {
     fun andClause(condition: String?, thenWhat: AndClause.() -> Unit) =
-            initComponent(AndClause(condition), thenWhat)
+        initComponent(AndClause(condition), thenWhat)
 }
 
 data class Optional(var description: String?) : Block()
 
-// ============ Back and Forths ============
-
-abstract class BackAndForth() : Inductive() {
-    fun withSync(
-            from: Actor,
-            to: Actor,
-            messageFrom: String,
-            messageTo: String,
-            thenWhat: WithSync.() -> Unit
-    ) = initComponent(WithSync(from, to, messageFrom, messageTo), thenWhat)
-
-    fun withSync(from: Actor, to: Actor, thenWhat: WithSync.() -> Unit) =
-            initComponent(WithSync(from, to, "calls", "replies"), thenWhat)
-
-    fun withAsync(
-            from: Actor,
-            to: Actor,
-            messageFrom: String,
-            messageTo: String,
-            thenWhat: WithAsync.() -> Unit
-    ) = initComponent(WithAsync(from, to, messageFrom, messageTo), thenWhat)
-
-    fun withAsync(from: Actor, to: Actor, thenWhat: WithAsync.() -> Unit) =
-            initComponent(WithAsync(from, to, "calls", "replies"), thenWhat)
-}
-
-class WithSync(var from: Actor?, var to: Actor?, var messageFrom: String?, var messageTo: String?) :
-        BackAndForth()
-
-class WithAsync(
-        var from: Actor?,
-        var to: Actor?,
-        var messageFrom: String?,
-        var messageTo: String?
-) : BackAndForth()
-
 // ============ Clauses ============
 
-abstract class Clause() : Inductive()
+    abstract class Clause() : Inductive()
 
 data class ElseClause(var condition: String?) : Clause()
 
 data class AndClause(var condition: String?) : Clause()
 
 // val sample = sequenceDiagram {
-//     autonumber()
-//     participants("alice", "bob", "john")
-//
-//     alternative("x=0") {
-//         callSync("alice", "bob", "hihi")
-//         replySync("bob", "alice", "hihi")
-//         elseClause("x=1") {
-//             callSync("alice", "bob", "hihi")
-//             replySync("bob", "alice", "hihi")
-//         }
-//         elseClause("x=2") {
-//             callSync("alice", "bob", "hihi")
-//             replySync("bob", "alice", "hihi")
-//         }
-//         elseClause("x=3") {
-//             callSync("alice", "bob", "hihi")
-//             replySync("bob", "alice", "hihi")
-//         }
-//     }
-//
-//     callSync("alice", "bob", "hihi")
-//     noteOver("alice", "alice says hihi")
-//     callSync("bob", "john", "hoho")
-//     noteOver("bob", "john", "bob says hoho")
-//
-//     parallel("Alice to Bob") {
-//         callSync("alice", "bob", "hihi")
-//         andClause("Bob to John") {
-//             callSync("bob", "john", "hihi")
-//         }
-//         andClause("Alice to John") {
-//             callSync("alice", "john", "hihi")
-//         }
-//     }
-//
-//     highlight("red") {
-//         optional("this is optional") {
-//             callSync("alice", "bob", "hihi")
-//             replySync("bob", "alice", "hihi")
-//         }
-//     }
-// }
+    //     autonumber()
+    //     participants("alice", "bob", "john")
+    //
+    //     alternative("x=0") {
+        //         callSync("alice", "bob", "hihi")
+        //         replySync("bob", "alice", "hihi")
+        //         elseClause("x=1") {
+            //             callSync("alice", "bob", "hihi")
+            //             replySync("bob", "alice", "hihi")
+            //         }
+            //         elseClause("x=2") {
+                //             callSync("alice", "bob", "hihi")
+                //             replySync("bob", "alice", "hihi")
+                //         }
+                //         elseClause("x=3") {
+                    //             callSync("alice", "bob", "hihi")
+                    //             replySync("bob", "alice", "hihi")
+                    //         }
+                    //     }
+                    //
+                    //     callSync("alice", "bob", "hihi")
+                    //     noteOver("alice", "alice says hihi")
+                    //     callSync("bob", "john", "hoho")
+                    //     noteOver("bob", "john", "bob says hoho")
+                    //
+                    //     parallel("Alice to Bob") {
+                        //         callSync("alice", "bob", "hihi")
+                        //         andClause("Bob to John") {
+                            //             callSync("bob", "john", "hihi")
+                            //         }
+                            //         andClause("Alice to John") {
+                                //             callSync("alice", "john", "hihi")
+                                //         }
+                                //     }
+                                //
+                                //     highlight("red") {
+                                    //         optional("this is optional") {
+                                        //             callSync("alice", "bob", "hihi")
+                                        //             replySync("bob", "alice", "hihi")
+                                        //         }
+                                        //     }
+                                        // }
